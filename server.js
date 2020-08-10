6651fa1207f02faf127a6d3cf6dbf9985ac64de5
Duha-Saleh <duhamahmoud096@gmail.com>
2020-08-10 15:03:55 +0300

    3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const agent = require('superagent');
+const superagent = require('superagent');
 
 const app = express();
 app.use(cors());
@@ -12,8 +12,12 @@ const PORT = process.env.PORT || 3000;
 //
 app.use(express.static('./public'));
 app.use(express.urlencoded({extended: true}));
+//to make cutting for the paths (no.ejs)
 app.set('view engine','ejs');
+app.use(express.json());
 
+//for the css
+app.use(express.static('./public'));
 
 //put new.ejs content when localhost:3000/search/new
 app.get('/search/new',(req,res)=>{
@@ -21,20 +25,42 @@ app.get('/search/new',(req,res)=>{
 });
 
 //put the content from API (https://developers.google.com/books/docs/v1/using ) 
-//by choosing from them using constructor when localhost:3000/searches
-
-server.post('/searches',(req,res)=>{
-  var  url = `https://www.googleapis.com/books/v1/volumes?q=${searchKey}+intitle`;
+//by choosing from them using constructor when localhost:3000/search/new we see the what is inside new.ejs اللي فيها الفورم 
+//وبس أختار في السيرش والتايتل او الاوثر بتطلع 
+//lacalhost:3000/searches اللي فيها
+// الconstructorالمحتويات تاعت  مرتبة
+app.get('/search/new',(req,res)=>{
+    res.render('./pages/searches/new');
 });
 
+app.post('/searches', function (req, res) {  
+    //requests from the body from new.ejs about name = ..... 
+    let bookSearch = req.body.bookSearch;
+    let searchType = req.body.searchType;
+    let maxresults = 10;
+  
+    let url = `https://www.googleapis.com/books/v1/volumes?q=in${bookSearch}:${searchType}&maxResults=${maxresults}`;
+    
+    
+    superagent.get(url)
+    .then(data => {
+        //becuase it's array
+    let x = data.body.items.map(element => {
+        return new Book(element);
+      })
+      res.render('./pages/searches/show', {arraybooks: x});
+    })
+  });
+
+
 function Book(data) {
-    this.bookImage = data.volumeInfo.imageLinks.thumbnail || data.volumeInfo.imageLinks.large || "https://i.imgur.com/J5LVHEL.jpg" ;
-    this.bookName = data.volumeInfo.title,
-    this.bookAuthor = data.volumeInfo.authors,
-    this.bookDesc = data.volumeInfo.description
+    this.bookImage = data.volumeInfo.imageLinks.thumbnail || "https://i.imgur.com/J5LVHEL.jpg" ;
+    this.bookName = data.volumeInfo.title || 'N/A';
+    this.bookAuthor = data.volumeInfo.authors || 'N/A'; 
+    this.bookDesc = data.volumeInfo.description || 'N/A';
 };
 //index.ejs is the home when localhost:3000
-server.get('/',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('./pages/index.ejs');
 });
 
